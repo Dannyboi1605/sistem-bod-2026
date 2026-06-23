da881db269dfc5a65cbd761d2263a753cfebce8c
Dannyboi1605 <mdafiqdanial16@gmail.com>
2026-06-23 12:15:34 +0800

    add admins through seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -9,6 +9,7 @@ class AdminSeeder extends Seeder
 {
     public function run()
     {
+        // 1. Superadmin (Developer Account)
         User::updateOrCreate(
             ['email' => 'mdafiqdanial16@gmail.com'],
             [
@@ -16,6 +17,42 @@ class AdminSeeder extends Seeder
                 'position' => 'Urus Setia / Admin',
                 'agency' => 'Kementerian Kewangan',
                 'roles' => ['admin', 'jawatankuasa'],
+                'password' => '@Bod2026',
+            ]
+        );
+
+        // 2. SITI NORHIDAYAH BINTI MUSLI (Admin & Urus Setia)
+        User::updateOrCreate(
+            ['email' => 'Hidayah.Musli@sabah.gov.my'],
+            [
+                'name' => 'SITI NORHIDAYAH BINTI MUSLI',
+                'position' => 'Urus Setia / Admin',
+                'agency' => 'Kementerian Kewangan',
+                'roles' => ['admin', 'jawatankuasa'],
+                'password' => '@Bod2026',
+            ]
+        );
+
+        // 3. NUR KHAIREENA BINTI FADZLEE (Admin & Urus Setia)
+        User::updateOrCreate(
+            ['email' => 'khaireena.fadzlee@sabah.gov.my'],
+            [
+                'name' => 'NUR KHAIREENA BINTI FADZLEE',
+                'position' => 'Urus Setia / Admin',
+                'agency' => 'Kementerian Kewangan',
+                'roles' => ['admin', 'jawatankuasa'],
+                'password' => '@Bod2026',
+            ]
+        );
+
+        // 4. MINA WARAH KAHAR (Urus Setia)
+        User::updateOrCreate(
+            ['email' => 'minawarah.kahar@sabah.gov.my'],
+            [
+                'name' => 'MINA WARAH KAHAR',
+                'position' => 'Urus Setia',
+                'agency' => 'Kementerian Kewangan',
+                'roles' => ['jawatankuasa'],
                 'password' => '@Bod2026',
             ]
         );
